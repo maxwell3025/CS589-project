70a2dacd2924de16ecd98639302b662ef7044d99
Maxwell Tang <amoinus@gmail.com>
2025-05-07 16:33:16 -0400

    Added help to plot generation tool

--- maxwell/plot_data.py
+++ maxwell/plot_data.py
@@ -3,10 +3,13 @@ from matplotlib import pyplot
 import argparse
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument("--data", required=True)
-argparser.add_argument("--index", required=True)
-argparser.add_argument("--name", required=True)
-
+argparser.add_argument("--data",  required = True, help = "The path to the .csv file whose data "
+                                                          "will be graphed.")
+argparser.add_argument("--index", required = True, help = "The column that will be the x axis for "
+                                                          "every graph.")
+argparser.add_argument("--name",  required = True, help = "A prefix for all of the generated file "
+                                                          "names.")
+ 
 args = argparser.parse_args()
 
 index = args.index
